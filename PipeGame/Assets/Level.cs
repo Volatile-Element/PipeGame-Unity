01c23331c09933f98ecb46b413f386d793cebb75
Syraneth <peterfkeen@hotmail.com>
2015-02-15 18:58:54 +0000

    Correction made

---
+++ PipeGame/Assets/Level.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+	public string[,] levelData;
+	
+	public int startNodeLoc;
+	public int endNodeLoc;
+	
+	public int numOfStraight;
+	public int numOfCorner;
+	public int numOfThree;
+	public int numOfFour;
+	public int numOfBridge;
+	public int numOfCornerBridge;
+	public int numOfPumpOne;
+
+}
--- PipeGame/Assets/Scripts/ReadWrite.cs
+++ PipeGame/Assets/Scripts/ReadWrite.cs
@@ -2,17 +2,15 @@
 using System.Collections;
 
 public class ReadWrite : MonoBehaviour {
-	public string[,] levelData;
 
-	public int startNodeLoc;
-	public int endNodeLoc;
-
-	public int numOfStraight;
-	public int numOfCorner;
-	public int numOfThree;
-	public int numOfFour;
-	public int numOfBridge;
-	public int numOfCornerBridge;
-	public int numOfPumpOne;
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
 }
